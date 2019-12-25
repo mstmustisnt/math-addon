d025debf9cf625b7e70a365fda5d47b2a3171600
Yevheniia Rakhmatova <yevheniia.rakhmatova@gmail.com>
2019-12-25 10:17:14 +0200

    Add readable test names

--- tests/index.js
+++ tests/index.js
@@ -2,12 +2,12 @@ const assert = require('assert');
 
 const math = require('bindings')('mathaddon.node');
 
-assert(math.abs(2) === 2);
-assert(math.abs(-2) === 2);
-assert(math.clz32(0) === 32);
-assert(math.clz32(1) === 31);
-assert(math.clz32(11) === 28);
-assert(math.clz32(7) === 29);
+assert(math.abs(2) === 22, 'math.abs(2) is not 2');
+assert(math.abs(-2) === 2, 'math.abs(-2) is not 2');
+assert(math.clz32(0) === 32, 'math.clz32(0) is not 32');
+assert(math.clz32(1) === 31, 'math.clz32(1) is not  31');
+assert(math.clz32(11) === 28, 'math.clz32(11) is not  28');
+assert(math.clz32(7) === 29, 'math.clz32(7) is not  29');
 assert.throws(math.clz32.bind(math,'string'), {
     name: 'TypeError',
     message: 'Wrong arguments',
